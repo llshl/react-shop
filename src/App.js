efd4d6965db2cb871a855d312b597ccec9bd8a02
llshl <lsh970708@naver.com>
2022-06-22 00:45:31 +0900

    component 반복생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button, Navbar, Container, Nav} from 'react-bootstrap';
+import {useState} from "react";
+import data from './data';
 
 function App() {
+
+    let [shoes] = useState(data)
   return (
     <div className="App">
       <Button variant="primary">Primary</Button>
@@ -16,8 +20,31 @@ function App() {
                 </Nav>
             </Container>
         </Navbar>
+
+        <div className="main-bg"></div>
+        <div className="container">
+            <div className="row">
+                {
+                    // 반복문으로 만들어놓은 component 생성
+                    shoes.map((shoe, index) => {
+                        return (<Card shoes={shoe} i={++index}></Card>)
+                    })
+                }
+            </div>
+        </div>
     </div>
   );
+}
+
+// component
+function Card(props){
+    return(
+        <div className="col-md-4">
+            <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%"/>
+            <h4>{props.shoes.title}</h4>
+            <p>{props.shoes.price}</p>
+        </div>
+    )
 }
 
 export default App;
