76d4d230c42e64a172cd8823f8b19aaf71cda87b
llshl <lsh970708@naver.com>
2022-07-01 02:08:47 +0900

    axios 사용과 로딩바

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import data from './data';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './routes/Details';
 import Card from './components/Cards';
+import axios from 'axios';
+import Loader from './components/Loader';
 
 function App() {
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data);
+  let [showCount, setShowCount] = useState(2);
+  let [loading, setLoading] = useState(false);
   let navigate = useNavigate();
   return (
     <div className="App">
+      {showCount}
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Shoes Shop</Navbar.Brand>
@@ -62,7 +67,7 @@ function App() {
           path="/detail/:id"
           element={
             <div>
-              <Detail shoes={shoes}/>
+              <Detail shoes={shoes} />
             </div>
           }
         />
@@ -75,6 +80,40 @@ function App() {
 
         <Route path="*" element={<dev>없는 페이지다 빡대가리야</dev>} />
       </Routes>
+      <div>
+        {loading ? (
+          <Loader type="spin" color="RGB 값" message="불러오는중" />
+        ) : null}
+      </div>
+
+      <Button
+        onClick={() => {
+          setLoading(true);
+          if (showCount === 4) {
+            alert('상품 이제 없다');
+            return;
+          }
+          // 로딩바를 잘 보기위해 1초 딜레이를 줬다
+          setTimeout(() => {
+            axios
+              .get(`https://codingapple1.github.io/shop/data${showCount}.json`)
+              .then((result) => {
+                setShowCount(showCount + 1);
+                //setLoading(false);
+                let copy = [...shoes, ...result.data];
+                setShoes(copy);
+
+                setLoading(false);
+              })
+              .catch((e) => {
+                console.log(e);
+              });
+          }, 1000);
+        }}
+      >
+        {' '}
+        버튼{' '}
+      </Button>
     </div>
   );
 }
